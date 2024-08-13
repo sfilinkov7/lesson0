72ea52e360730e06bee987fc33c000d66014fabf
sfilinkov7 <sfilinkov7@yandex.ru>
2024-08-13 20:20:02 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+# 1st program
+print ((9 ** 0.5)*5)
+
+# 2and program
+print (9.99 > 9.98, 1000 != 1000.1)
+
+# 3rd program
+print (2 * 2 + 2)
+print (2 * (2 + 2))
+print ((2 * 2 + 2) == (2 * (2 + 2)))
+
+# 4th program
+print(type(float('123.456')))
+print(int(123.456 * 10 % 10))
+
+
+
